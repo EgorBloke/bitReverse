2c2b4408d938ef03d1d045b6c2c0027a5204fc1f
EgorBloke <75785332+EgorBloke@users.noreply.github.com>
2021-07-28 15:20:01 +0300

    Add files via upload

--- BitInverse.cpp
+++ BitInverse.cpp
@@ -3,36 +3,38 @@
 
 #include <iostream>
 #include<bitset>
-void bitInverse(uint8_t& val);
+uint8_t bitInverse(uint8_t val);
 void printByte(uint8_t& val);
 int main()
 {
 
-	uint8_t byte = 0b10101111;
-	printByte(byte);
-	bitInverse(byte);
-	printByte(byte);
+	uint8_t byte = 0b10101111;	//пробный байт
+	printByte(byte);	
+	uint8_t byte_edited = bitInverse(byte);
+	printByte(byte_edited);
 	return 0;
 }
 
-void bitInverse(uint8_t &val) {
-	for (int i = 0; i < 4; ++i) {
+uint8_t bitInverse(uint8_t val) 
+{
+	for (int i = 0; i < 4; ++i)		//цикл до полбайт
+	{
 		if (bool(val & (1 << i)) != bool(val & (1 << (7 - i)))) //
 		{
-			bool buf = static_cast<bool>(val & (1 << i));
-			if (buf) // ==1
+			bool buf = static_cast<bool>(val & (1 << i));		//значение текущего бита
+			if (buf) // если == 1
 			{
-				val &= ~(1 << i);
-				val |= (1 << (7 - i));
+				val &= ~(1 << i);	//устанавливаем значение в 0
+				val |= (1 << (7 - i));	//устанавливаем значение в 1
 			}
-			else
+			else  //если ==0, то наоборот: текущий бит в 1, зекркальный - в 0
 			{
-				val |= 1 << (7 - i);
+				val |= 1 << (7 - i);		
 				val &= 1 << i;
 			}
 		}
 	}
-	return;
+	return val;
 }
 void printByte(uint8_t& val) {
 	std::cout << static_cast<int>(val) << std::endl;
